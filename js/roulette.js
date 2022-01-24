dee567a4bb61a40f95d40e5f3a1dc198c00641d7
Hyeonmin Koo <hyeon6423@naver.com>
2022-01-25 01:35:30 +0900

    댕냥체 v0.1.1

--- js/roulette.js
+++ js/roulette.js
@@ -1,8 +1,40 @@
-let container1 = document.querySelector(".container1");
-let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 1000);
+let degree = 1800;
+let clicks = 0;
 
-btn.onclick = function () {
-	container1.style.transform = "rotate(" + number + "deg)";
-	number += Math.ceil(Math.random() * 1000);
-}
+$(document).ready(function() {
+  $('#spin').click(function() {
+    
+    clicks ++;
+    var newDegree = degree * clicks;
+    var extraDegree = Math.floor(Math.random() * (360 - 1 + 1)) + 1;
+    totalDegree = newDegree + extraDegree;
+    
+    $('#wheel .sec').each(function() {
+      var t = $(this);
+      var noY = 0;
+      
+      var c = 0;
+      var n = 700;
+      var interval =  setInterval(function () {
+        c++;
+        if (c === n) {
+          clearInterval(interval);
+        }
+        
+        var aoY = t.offset().top;
+        $('#txt').html(aoY);
+        
+        if(aoY < 23.89) {
+          $('#spin').addClass('spin');
+          setTimeout(function () {
+            $("#spin").removeClass('spin');
+          }, 100);
+        }
+      }, 10);
+      
+      $('#inner-wheel').css({'transform' : 'rotate(' + totalDegree + 'deg)'});
+      
+      noY = t.offset().top;
+    });
+  });
+});
